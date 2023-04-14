8fe0825d1bd81412f081a57acac71c6b96b8cd4b
habibu <habib.az@hotmail.com>
2023-04-14 05:17:50 +0200

    feats: add splash screen aka launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 import 'kalolin_abinci.dart';
 import 'kayan_girki.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyAbinci());
+  FlutterNativeSplash.remove();
 }
 
 class MyAbinci extends StatelessWidget {
