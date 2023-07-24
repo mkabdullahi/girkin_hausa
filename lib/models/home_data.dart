6cd3c98bb53e9be1dcff1a85035a3f3849d88530
habibu <habib.az@hotmail.com>
2023-07-24 08:59:34 +0200

    feat: add sample json data

--- lib/models/home_data.dart
+++ lib/models/home_data.dart
@@ -2,11 +2,11 @@ import 'models.dart';
 
 class HomeData {
   //TODO: implement HomeAbici
-  final List<HomeAbinci> yawuAbaka;
+  //final List<HomeAbinci> yawuAbaka;
   final List<AbokanGirki> abokanGirke;
 
   HomeData(
-      this.yawuAbaka,
+      //this.yawuAbaka,
       this.abokanGirke
       );
 }
